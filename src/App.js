91a165d6555217fd325edd62f0b1c8bb4195bd43
Himanshu <mehra3228@gmail.com>
2023-01-26 14:00:13 +0530

    Basics 1 JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  // it return a jsx
+  // jsx allows ius to write the html in the react
+  const title = "blog Post";
+  const body = "this is my blog post";
+
+  const comments = [
+    { id: 1, text: "Home" },
+    { id: 2, text: "contact" },
+    { id: 3, text: "about us" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+  const commentBlock=(
+        <div className="comments">
+          <h1>{comments.length}</h1>
+          <ul>
+            {comments.map((item) => {
+              return <li key={item.text + item.id}>{item.text}</li>;
+            })}
+          </ul>
+        </div>
+      )
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{showComments && title}</h1>
+      <p>{body}</p>
+      {showComments ? commentBlock : (
+        "no"
+      )}
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+// allows us to interact with the DOM in the browser
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// render() takes two things first the element and seond where you want to insert it
+//here app component is the root
+// ReactDOM.render(
+//     // it active some additonal checks and warnigs
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
